e806c9c68f76eb214a0067d66ca073eaedef9363
Ana Paola Montero <anapao.montero@gmail.com>
2018-06-06 20:56:03 -0400

    develop: Adding CRUD Interface

---
+++ Base/Base/Crudable.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Base
+{
+    public interface Crudable
+    {
+        bool Create(Object obj);
+        List<Object> Get();
+        bool Update(String key, Object obj);
+        bool Delete(String key);
+    }
+}
