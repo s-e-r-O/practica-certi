e952da346402ee52ddf5e9fe69fa2fd6cbc64b54
Carlos Gamboa <carlo-x-17@hotmail.com>
2018-06-07 22:27:46 -0400

    develop: Added a class who will act as our database.

---
+++ Base/Base/Database.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Base
+{
+    class Database
+    {
+        private static Database _instance;
+
+        private Database() { }
+
+        public static Database Instance
+        {
+            get
+            {
+                if(Instance == null)
+                {
+                    Instance = new Database();
+                }
+                return Instance;
+            }
+        }
+
+        public List<Product> ProductsList { get; set; }
+        public List<Category> CategorysList { get; set; }
+        public List<Store> StoresList { get; set; }
+        public List<ShippingAddress> ShippingAddressesList { get; set; }
+        public List<User> UsersList { get; set; }
+        public List<ProductCart> ProductCartsList { get; set; }
+        public List<Cart> CartsList { get; set; }
+
+
+    }
+}
--- Base/Base/Program.cs
+++ Base/Base/Program.cs
@@ -10,6 +10,8 @@ namespace Base
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("This is an E-Commerce App!!");
+            Console.ReadLine();
         }
     }
 }
