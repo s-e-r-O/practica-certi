b5b50803107ae2a29ef70cabfb571d2f406f39dd
Eun Kyu Choi <choi2nkyu@gmail.com>
2018-06-10 18:26:08 -0400

    FE-004: added Delete and Get

--- Base/Base/ShippingAddressService.cs
+++ Base/Base/ShippingAddressService.cs
@@ -8,6 +8,7 @@ namespace Base
 {
     class ShippingAddressService : ICrudable<ShippingAddress>
     {
+        //change _dbContext to User in the end
         private static ECommerceDB _dbContext = ECommerceDB.Instance;
         public User User { private get; set; }
         
@@ -27,18 +28,35 @@ namespace Base
                 Console.WriteLine("The user '" + "' already has the shipping address of '" + obj.Identifier);
                 return false;
             }
-            //add to user shipping address list
+            _dbContext.ShippingAddressesList.Add(obj);
             return true;
         }
 
         public bool Delete(string key)
         {
-            throw new NotImplementedException();
+            if (User == null)
+            {
+                Console.WriteLine("No cart was specified.");
+                return false;
+            }
+            int index;
+            if ((index = _dbContext.ShippingAddressesList.FindIndex(shippingAddress => { return shippingAddress.Identifier == key; })) < 0)
+            {
+                Console.WriteLine("The user '" + "' does not have the shipping address of '" + obj.Identifier);
+                return false;
+            }
+            _dbContext.ShippingAddressesList.RemoveAt(index);
+            return true;
         }
 
         public List<ShippingAddress> Get()
         {
-            throw new NotImplementedException();
+            if (User == null)
+            {
+                Console.WriteLine("No user specified");
+                return null;
+            }
+            return _dbContext.ShippingAddressesList;
         }
 
         public bool Update(string key, ShippingAddress obj)
