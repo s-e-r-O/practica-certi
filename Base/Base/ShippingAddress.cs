b8d60b0d3ca9d9aa60133cb0eeb196592c96917f
Eun Kyu Choi <choi2nkyu@gmail.com>
2018-06-10 14:02:19 -0400

    FE-004: shipping address service class created

--- Base/Base/ShippingAddress.cs
+++ Base/Base/ShippingAddress.cs
@@ -8,5 +8,11 @@ namespace Base
 {
     class ShippingAddress
     {
+        public string Identifier { get; set; }
+        public string Line1 { get; set; }
+        public string Line2 { get; set; }
+        public int Phone { get; set; }
+        public string City { get; set; }
+        public string Zone { get; set; }
     }
 }
