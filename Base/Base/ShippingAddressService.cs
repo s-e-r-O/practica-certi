ee8a1bafac2851a8a9e75b8c09e6b4fe5c554ae4
Eun Kyu Choi <choi2nkyu@gmail.com>
2018-06-10 14:02:19 -0400

    FE-004: shipping address service class created

---
+++ Base/Base/ShippingAddressService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Base
+{
+    class ShippingAddressService : Crudable
+    {
+        public bool Create(object obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Delete(string key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<object> Get()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Update(string key, object obj)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
