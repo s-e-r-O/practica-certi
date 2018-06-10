5a764509ef45eaeccaa0dd67ddf97594e93c5fbe
Sergio Oporto Tejerina <ser.op.te@gmail.com>
2018-06-10 07:13:59 -0400

    FE-006: Finished the ProductCart model

--- Base/Base/ProductCart.cs
+++ Base/Base/ProductCart.cs
@@ -6,7 +6,25 @@ using System.Threading.Tasks;
 
 namespace Base
 {
+    enum ShippingDelivery {
+        Express,
+        Normal,
+        InStore,
+        Free,
+        None
+    }
+
     class ProductCart
     {
+        public string ProductCode { get; set; }
+        public string SelectedDelivery { get; set; }
+        public Store Store { get; set; }
+        public int Quantity { get; set; }
+
+        public void Show()
+        {
+            Console.Write("PRODUCT CART:");
+            Console.Write("Code: " + ProductCode + " Delivery: " + SelectedDelivery + " Quantity: " + Quantity);
+        }
     }
 }
