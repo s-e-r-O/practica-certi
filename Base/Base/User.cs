543f274b2ab306906d4bb915dfff0540e4155f15
Smitty <fabio_urca@hotmail.com>
2018-06-10 21:31:36 -0400

    FE-005: added function in user and added userservice.cs

--- Base/Base/User.cs
+++ Base/Base/User.cs
@@ -6,7 +6,30 @@ using System.Threading.Tasks;
 
 namespace Base
 {
-    class User
+    class User 
     {
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+
+        private List<ShippingAddress> _shipAdress;
+        public List<ShippingAddress> ShipAdress
+        {
+            get
+            {
+                if(_shipAdress == null)
+                {
+                    _shipAdress = new List<ShippingAddress>();
+                }
+                return _shipAdress;
+            }
+            private set { }
+        }
+
+        public void Show()
+        {
+           
+        }
     }
 }
