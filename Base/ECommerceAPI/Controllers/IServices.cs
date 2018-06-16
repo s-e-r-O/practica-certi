029a7ab577085501f38b4c9966140e4681727d0c
choquemeneses7 <veimar.xd@hotmail.com>
2018-06-15 20:56:27 -0400

    API: Added Interface to API Services

---
+++ Base/ECommerceAPI/Controllers/IServices.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerceAPI.Controllers
+{
+    public interface IServices
+    {
+        HttpResponseMessage Get(int id);
+        HttpResponseMessage Get();
+        HttpResponseMessage Post(Object content);
+        HttpResponseMessage Put(Object res);
+        HttpResponseMessage Delete(Object id);
+    }
+}
