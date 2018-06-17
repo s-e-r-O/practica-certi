3d34e3851214827c45e0d22412c2aaa55b6b6bf5
Sergio Oporto Tejerina <ser.op.te@gmail.com>
2018-06-17 16:39:39 -0400

    UI: Added Router Module

--- base-ui/src/app/app.module.ts
+++ base-ui/src/app/app.module.ts
@@ -2,13 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './/app-routing.module';
+import { TestComponent } from './test/test.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TestComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
