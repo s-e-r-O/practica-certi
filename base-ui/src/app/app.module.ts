23a7a3a8d41a3dbbc6d7eedef4a416a38fd8ad7d
Sergio Oporto Tejerina <ser.op.te@gmail.com>
2018-06-18 00:25:46 -0400

    UI: Added bootstrap and ng-bootstrap

--- base-ui/src/app/app.module.ts
+++ base-ui/src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './/app-routing.module';
 import { TestComponent } from './test/test.component';
@@ -12,7 +14,8 @@ import { TestComponent } from './test/test.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
