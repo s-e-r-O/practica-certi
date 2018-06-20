02e99404eda2d3c339e4166e3700a3d7195900d3
Sergio Oporto Tejerina <ser.op.te@gmail.com>
2018-06-20 12:29:06 -0400

    UI: Added styles to store page

--- base-ui/src/app/services/product.service.ts
+++ base-ui/src/app/services/product.service.ts
@@ -16,8 +16,8 @@ export class ProductService {
         description: "AAAA",
         type: "Digital",
         delivery: "None",
-        price: "4.99$",
-        imageURL: "http://lorempixel.com/100/100",
+        price: "$4.99",
+        imageURL: "https://images-na.ssl-images-amazon.com/images/I/514GfnjbUCL._SX355_.jpg",
         category: new Category({ name: "Category", description: "BBB"})
       }),
       new Product({
@@ -26,7 +26,48 @@ export class ProductService {
         description: "AAAA",
         type: "Digital",
         delivery: "None",
-        imageURL: "http://lorempixel.com/100/100",
+        price: "$4.99",
+        imageURL: "https://store.storeimages.cdn-apple.com/4666/as-images.apple.com/is/image/AppleInc/aos/published/images/i/ph/iphone/x/iphone-x-silver-select-2017?wid=305&hei=358&fmt=jpeg&qlt=95&op_usm=0.5,0.5&.v=1515602510472",
+        category: new Category({ name: "Category", description: "BBB"})
+      }),
+      new Product({
+        code: "123", 
+        name: "Product1", 
+        description: "AAAA",
+        type: "Digital",
+        delivery: "None",
+        price: "$4.99",
+        imageURL: "https://images-na.ssl-images-amazon.com/images/I/514GfnjbUCL._SX355_.jpg",
+        category: new Category({ name: "Category", description: "BBB"})
+      }),
+      new Product({
+        code: "456", 
+        name: "Product2", 
+        description: "AAAA",
+        type: "Digital",
+        delivery: "None",
+        price: "$4.99",
+        imageURL: "https://store.storeimages.cdn-apple.com/4666/as-images.apple.com/is/image/AppleInc/aos/published/images/i/ph/iphone/x/iphone-x-silver-select-2017?wid=305&hei=358&fmt=jpeg&qlt=95&op_usm=0.5,0.5&.v=1515602510472",
+        category: new Category({ name: "Category", description: "BBB"})
+      }),
+      new Product({
+        code: "123", 
+        name: "Product1", 
+        description: "AAAA",
+        type: "Digital",
+        delivery: "None",
+        price: "$4.99",
+        imageURL: "https://images-na.ssl-images-amazon.com/images/I/514GfnjbUCL._SX355_.jpg",
+        category: new Category({ name: "Category", description: "BBB"})
+      }),
+      new Product({
+        code: "456", 
+        name: "Product2", 
+        description: "AAAA",
+        type: "Digital",
+        delivery: "None",
+        price: "$4.99",
+        imageURL: "https://store.storeimages.cdn-apple.com/4666/as-images.apple.com/is/image/AppleInc/aos/published/images/i/ph/iphone/x/iphone-x-silver-select-2017?wid=305&hei=358&fmt=jpeg&qlt=95&op_usm=0.5,0.5&.v=1515602510472",
         category: new Category({ name: "Category", description: "BBB"})
       })
     ]);
