1f6ebe82aceb81a287f30ece543d9c55bc3c3af4
choquemeneses7 <veimar.xd@hotmail.com>
2018-06-20 01:01:26 -0400

    API: UserController direction changed

--- Base/ECommerceAPI/Controllers/UserController.cs
+++ Base/ECommerceAPI/Controllers/UserController.cs
@@ -9,7 +9,7 @@ using System.Net.Http;
 using System.Text;
 using System.Web.Http;
 
-namespace ECommerceAPI
+namespace ECommerceAPI.controllers
 {
     public class UserController : ApiController, IServices
     {
